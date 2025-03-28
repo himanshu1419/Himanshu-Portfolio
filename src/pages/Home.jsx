82509590d453861abf502739feb44346ea6eb348
himanshunegi1419 <himanshunegi1419@gmail.com>
2025-03-28 11:13:01 +0530

    Add images,pdf and other files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Hero from "../components/Hero";
+import { motion } from "framer-motion";
+import About from "./About";
+import Resume from "./Resume";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+  <div>
+    <Hero />
 
-export default Home
+    {/* About Section */}
+    <section id="about" className="py-16 px-6 bg-gray-800">
+        <About />
+      </section>
+
+      {/* Resume Section */}
+      <section id="resume" className="py-16 px-6 bg-gray-800">
+        <Resume />
+      </section>
+
+      {/* Projects Section */}
+      <section id="projects" className="py-16 px-6 bg-gray-800">
+        <Projects />
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact" className="py-16 px-6 bg-gray-800">
+        <Contact />
+      </section>
+    </div>
+  )
+  
+};
+
+export default Home;
